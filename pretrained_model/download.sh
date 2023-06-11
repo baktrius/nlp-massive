b3fdd6778e999669cbbabd176d87033936aa616a
Krzysztof Rogowski <krzysztor99@gmail.com>
2023-06-11 11:05:35 +0200

    polish finetunning

---
+++ pretrained_model/download.sh
@@ -0,0 +1,2 @@
+wget https://huggingface.co/xlm-roberta-base/resolve/main/sentencepiece.bpe.model
+wget https://huggingface.co/xlm-roberta-base/resolve/main/pytorch_model.bin
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+PYTHONPATH=src/ python scripts/train.py -c config.yml
